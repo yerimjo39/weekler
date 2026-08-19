a42abec0706b77573e2ea1e2227f4e750d163f0f
조예림 <kim.daisy1001@gmail.com>
2026-08-20 03:51:30 +0900

    화면 전환, 일정 추가 및 편집 외 수정

--- html/script.js
+++ html/script.js
@@ -1,21 +1,82 @@
-const checkbox = document.querySelector('.schedule-area input[type="checkbox"]');
+/*1. 저장, 불러오기*/
 
-checkbox.addEventListener('change', function () {
-    this.closest('li').classList.toggle('completed', this.checked);
+/*2. 화면 그리기(랜더링)*/
+
+/*3.사용자 반응*/
+
+/*3-1. 페이지 전환*/
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header')
+    if(!header) return;
+
+    header.addEventListener('click', (e) => {
+        const btn = e.target.closest('button');
+        if (!btn) return;
+
+        if (btn.classList.contains('mon')) {
+            location.href = 'month.html';
+        } else if (btn.classList.contains('week')) {
+            location.href = 'index.html';
+        } else if (btn.classList.contains('todo')) {
+            location.href = 'todo.html';
+        }
+    })
+})
+
+const calendar = document.querySelector('#calendar');
+
+/*3-2. 일정 추가 및 편집*/
+calendar.addEventListener('click', function(e) {
+    //더보기 메뉴
+    if (e.target.matches('.schedule-area li > button')) {
+        const deleteMenu = e.target.nextElementSibling;
+        deleteMenu.classList.toggle('show');
+        return;
+    }
+
+    //일정 삭제
+    if (e.target.closest('.delete-schedule button')) {
+        const li = e.target.closest('li');
+        li.remove();
+        return;
+    }
+    
+    //일정 추가
+    const scheduleArea = e.target.closest('.schedule-area');
+    if (!scheduleArea) return;          
+    if (e.target.closest('li')) return;
+
+    addNewSchedule(scheduleArea);
 });
 
-document.querySelectorAll('.schedule-area input[type="checkbox"]').forEach(function(checkbox) {
+function addNewSchedule(scheduleArea) {
+    const ul = scheduleArea.querySelector('ul');
 
-    checkbox.addEventListener('change', function() {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <input type="checkbox">
+        <span contenteditable="true"></span>
+        <button>⋮</button>
+        <div class="delete-schedule">
+            <button>일정 삭제</button>
+        </div>
+    `;
 
-        const li = this.closest('li');
+    ul.prepend(li);
 
-        if (this.checked) {
-            li.classList.add('completed');
-        } else {
-            li.classList.remove('completed');
+    const span = li.querySelector('span');
+    span.focus();
+
+    span.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            span.blur();
         }
-
     });
 
-});
+    span.addEventListener('blur', function() {
+        if (span.textContent.trim() === '') {
+            li.remove();
+        }
+    }, { once: true });
+}
